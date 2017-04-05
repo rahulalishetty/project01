d1845a47fcd63446a51a25d7f9fe47b48c489ffb
kalariya-parikshith <k.parikshith93@gmail.com>
2017-04-05 18:53:14 +0530

    Create wait.php

---
+++ wait.php
@@ -0,0 +1,13 @@
+<style type="text/css">
+.cen{
+	text-align: center;
+	margin-top: 300;
+}
+</style>
+<div class="cen">
+<img src="Loading_bar.gif">
+</div>
+<?php
+
+header( "refresh:3;url=yeslogin.php" );
+?>
